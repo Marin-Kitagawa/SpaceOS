649f5849390c98007b420094f951178a94d5f89b
The Rubyist <49131888+ruby-rust-perl@users.noreply.github.com>
2021-11-14 15:05:46 +0530

    Cleaned up boot.asm

--- boot.asm
+++ boot.asm
@@ -9,19 +9,6 @@ times 33 db 0                       ; For rest of the BIOS parameter blocks i.e.
 start:
     jmp 0x7c0:step                  ; Makes our code segment 0x7c0
                                     ; For more information on interrupts/exceptions, see https://wiki.osdev.org/Exceptions
-handle_zero:                        ; For handling interrupt 0x00 (official name); for more information on interrupts, see Ralf Brown's interrupt list
-    mov ah, 0eh
-    mov al, 'A'
-    mov bx, 0x00
-    int 0x10
-    iret
-    
-handle_one:
-    mov ah, 0eh
-    mov al, 'V'
-    mov bx, 0x00
-    int 0x10
-    iret
 
 step:
     cli                             ; Clear interrupts 
@@ -32,20 +19,6 @@ step:
     mov ss, ax
     mov sp, 0x7c00                  ; Stack grows downward
     sti                             ; Set (enable) interrupts
-   
-    mov word[ss:0x00], handle_zero  ; If ss is not specified, then it will take the data segment by default. But the interrupt 0x00 starts at 0x00 while ds points to 0x7c -> leads to a problem
-    mov word[ss:0x02], 0x7c0
-    
-    mov word[ss:0x04], handle_one
-    mov word[ss:0x06], 0x7c0
-
-    ; int 0                           ; For example, division by zero is an interrupt zero
-    ; mov ax, 0x00
-    ; div ax                          ; This divides ax by ax -> interrupt zero
-    int 1
-
-    mov si, message
-    call print
     jmp $
 print:
     mov bx, 0                       ; For the page number
@@ -64,10 +37,23 @@ print_char:
 
     ret
 
-message: db 'Hello, World!', 0
 times 510-($-$$) db 0
 dw 0xAA55
 
 ; This bootloader will somtimes tamper with the data because of the BIOS Parameter Block. Some BIOS will expect this to be present
 ; So we create a BIOS Parameter block for jmp short 3C nop. This is required to jump over the disk format information. This is required even in Non-bootable volumes (required as JMP in both Windows and OSX).
 ; Without the above jump, BIOS will attempt to load the data that isn't a code
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Reading from CHS (Cylindrical Head Sector)                                                ;;
+;; AH = 02h                                                                                  ;;
+;; AL = number of sectors to read                                                            ;;
+;; CH = low eight bits of cylinder number                                                    ;;
+;; CL = sector number 1-63 (bits 0-5) high two bits of cylinder (bits 6-7, hard disk only)   ;;
+;; DH = head number                                                                          ;;
+;; DL = drive number (bit 7 set for hard disk) drive number choosen automatically.           ;;
+;; ES:BX -> data buffer                                                                      ;;
+;; Returns:                                                                                  ;;
+;; CF (carry flag) set if there is an error or cleared on success                            ;;
+;; If AH = 11h (corrected ECC error) AL = burst length                                       ;;
+;; AH = status and AL = number of sectors transferred                                        ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
