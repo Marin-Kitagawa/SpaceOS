7ebee8984b8e37e69d6a71813907d0203a9ecbce
The Rubyist <49131888+ruby-rust-perl@users.noreply.github.com>
2021-11-14 14:21:07 +0530

    Interrupts 0, 1

--- boot.asm
+++ boot.asm
@@ -8,6 +8,21 @@ times 33 db 0                       ; For rest of the BIOS parameter blocks i.e.
 
 start:
     jmp 0x7c0:step                  ; Makes our code segment 0x7c0
+                                    ; For more information on interrupts/exceptions, see https://wiki.osdev.org/Exceptions
+handle_zero:                        ; For handling interrupt 0x00 (official name)
+    mov ah, 0eh
+    mov al, 'A'
+    mov bx, 0x00
+    int 0x10
+    iret
+    
+handle_one:
+    mov ah, 0eh
+    mov al, 'V'
+    mov bx, 0x00
+    int 0x10
+    iret
+
 step:
     cli                             ; Clear interrupts 
     mov ax, 0x7c0
@@ -17,6 +32,18 @@ step:
     mov ss, ax
     mov sp, 0x7c00                  ; Stack grows downward
     sti                             ; Set (enable) interrupts
+   
+    mov word[ss:0x00], handle_zero  ; If ss is not specified, then it will take the data segment by default. But the interrupt 0x00 starts at 0x00 while ds points to 0x7c -> leads to a problem
+    mov word[ss:0x02], 0x7c0
+    
+    mov word[ss:0x04], handle_one
+    mov word[ss:0x06], 0x7c0
+
+    ; int 0                           ; For example, division by zero is an interrupt zero
+    ; mov ax, 0x00
+    ; div ax                          ; This divides ax by ax -> interrupt zero
+    int 1
+
     mov si, message
     call print
     jmp $
